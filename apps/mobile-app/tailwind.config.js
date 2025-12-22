9eaf4bbf26b320069ee19876dda81442b15e68da
ltshikila <ltshikila17@gmail.com>
2025-12-22 11:51:40 +0200

    className fix

--- apps/mobile-app/tailwind.config.js
+++ apps/mobile-app/tailwind.config.js
@@ -1,13 +1,12 @@
-// apps/mobile-app/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-
   content: [
     "./*.{js,ts,tsx,jsx}",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./src/**/*.{js,jsx,ts,tsx}",
     "./index.{js,ts,tsx}"
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
